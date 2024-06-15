7548182a7a63ef7522b470740c2c712676c64f0e
Rohan Jacinto <rohanjacin@gmail.com>
2024-06-15 09:58:29 +0530

    Added public card value (qty,suite,rank) constraint checks with private values
    Signed-off-by: Rohan Jacinto <rohanjacin@gmail.com>

--- circuits/src/card.rs
+++ circuits/src/card.rs
@@ -2,7 +2,7 @@ use std::marker::PhantomData;
 use ff::{Field, PrimeField, PrimeFieldBits};
 use halo2_proofs::{
     circuit::{AssignedCell, Layouter, Value, SimpleFloorPlanner},
-    plonk::{Advice, Assigned, Column, ConstraintSystem,
+    plonk::{Advice, Instance, Assigned, Column, ConstraintSystem,
         Constraints, Error, Expression, Selector, Circuit},
     poly::Rotation,
     pasta::Fp,
@@ -14,6 +14,9 @@ struct CardConfig {
     qty: Column<Advice>,
     suite: Column<Advice>,
     rank: Column<Advice>,
+    pub_qty: Column<Instance>,
+    pub_suite: Column<Instance>,
+    pub_rank: Column<Instance>,
     s_check: Selector,
 }
 
@@ -34,9 +37,13 @@ impl<F: PrimeField> CardChip<F> {
     }
 
     fn configure(meta: &mut ConstraintSystem<F>, qty: Column<Advice>,
-                suite: Column<Advice>, rank: Column<Advice>) -> CardConfig {
+                suite: Column<Advice>, rank: Column<Advice>,
+                pub_qty: Column<Instance>, pub_suite: Column<Instance>,
+                pub_rank: Column<Instance>, s_check: Selector) -> CardConfig {
 
-        let s_check = meta.selector();
+        meta.enable_equality(pub_qty);
+        meta.enable_equality(pub_suite);
+        meta.enable_equality(pub_rank);
 
         let qty_check = |value: Expression<F>| {
             // Quantity check 1 or 2 or 3 or 4
@@ -74,61 +81,66 @@ impl<F: PrimeField> CardChip<F> {
                 let rank = meta.query_advice(rank, Rotation::cur());
 
                 // Public inputs
-/*                let pub_qty = meta.query_advice(pub_qty, Rotation::next());
-                let pub_suite = meta.query_advice(pub_suite, Rotation::next());
-                let pub_rank = meta.query_advice(pub_rank, Rotation::next());
-*/
+                let pub_qty = meta.query_instance(pub_qty, Rotation::cur());
+                let pub_suite = meta.query_instance(pub_suite, Rotation::cur());
+                let pub_rank = meta.query_instance(pub_rank, Rotation::cur());
+
                 // Card check is qty_check AND suite_check AND rank_check
-/*                let poly1 = pub_qty - qty.clone(); 
-                let poly2 = pub_suite - suite.clone(); 
-                let poly3 = pub_rank - rank.clone(); 
-*/
+                let qty_match = qty.clone() - pub_qty; 
+                let suite_match = suite.clone() - pub_suite; 
+                let rank_match = rank.clone() - pub_rank; 
+
                 Constraints::with_selector(s,
                         [
                             ("qty_check", qty_check(qty)),
                             ("suite_check", suite_check(suite)),
                             ("rank_check", rank_check(rank)),
-/*                            ("poly1", poly1),
-                            ("poly2", poly2),
-                            ("poly3", poly3),
-*/
+                            ("qty_match", qty_match),
+                            ("suite_match", suite_match),
+                            ("rank_match", rank_match),
                         ],
                 )
         });
 
+        //println!("\nCreategate done:{}", meta.get_instance_query_index(qty, Rotation::cur()));
         CardConfig {
             qty,
             suite,
             rank,
-/*            pub_qty,
+            pub_qty,
             pub_suite,
             pub_rank,
-*/            s_check,
+            s_check,
         }
     }
 
     pub fn assign(&self, mut layouter: impl Layouter<F>,
         qty: Value<Assigned<F>>, suite: Value<Assigned<F>>,
         rank: Value<Assigned<F>>) -> 
-        Result<(), Error> {
+        Result<(CardConstrained<F>, CardConstrained<F>,
+            CardConstrained<F>), Error> {
 
         let offset = 0;
 
         layouter.assign_region( || "Card", |mut region| {
             self.config.s_check.enable(&mut region, offset)?;
 
-            region.assign_advice(|| "qty value",
+            let qty_cell = region
+                .assign_advice(|| "qty value",
                 self.config.qty, offset, || qty)
-                .map(CardConstrained);
+                .map(CardConstrained).unwrap();
 
-            region.assign_advice(|| "suite value",
+            let suite_cell = region.
+                assign_advice(|| "suite value",
                 self.config.suite, offset, || suite)
-                .map(CardConstrained);
+                .map(CardConstrained).unwrap();
 
-            region.assign_advice(|| "rank value",
+            let rank_cell: CardConstrained<F> = region.
+                assign_advice(|| "rank value",
                 self.config.rank, offset, || rank)
-                .map(CardConstrained);
-            Ok(())
+                .map(CardConstrained).unwrap();
+
+            Ok((qty_cell, suite_cell, rank_cell))
         })
     }
 }
@@ -153,11 +165,13 @@ impl<F: PrimeField> Circuit<F> for CardCircuit<F> {
         let qty = meta.advice_column();
         let suite = meta.advice_column();
         let rank = meta.advice_column();
-/*        let pub_qty = meta.instance_column();
+        let pub_qty = meta.instance_column();
         let pub_suite = meta.instance_column();
         let pub_rank = meta.instance_column();
-*/
-        CardChip::configure(meta, qty, suite, rank)
+        let s_check = meta.selector();
+
+        CardChip::configure(meta, qty, suite, rank,
+                    pub_qty, pub_suite, pub_rank, s_check)
     }
 
     fn synthesize(&self, config: Self::Config, mut layouter: impl Layouter<F>) 
@@ -165,8 +179,9 @@ impl<F: PrimeField> Circuit<F> for CardCircuit<F> {
 
         let chip: CardChip<F> = CardChip::construct(config);
 
-        let _ = chip.assign(layouter.namespace(|| "Card Assign"),
-            self.qty, self.suite, self.rank);
+        let (qtycell, suitecell, rankcell) = chip
+            .assign(layouter.namespace(|| "Card Assign"),
+            self.qty, self.suite, self.rank).unwrap();
 
         Ok(())
     }
@@ -174,15 +189,20 @@ impl<F: PrimeField> Circuit<F> for CardCircuit<F> {
 
 #[test]
 fn test_range_check_1() {
-    const k: u32 = 5;
+    const k: u32 = 10;
 
     // Successful case
     let circuit = CardCircuit::<Fp> {
-        qty: Value::known(Fp::from(3 as u64).into()),
+        qty: Value::known(Fp::from(1 as u64).into()),
         suite: Value::known(Fp::from(2 as u64).into()),
-        rank: Value::known(Fp::from(0 as u64).into()),
+        rank: Value::known(Fp::from(3 as u64).into()),
     };
 
-    let prover = MockProver::run(k, &circuit, vec![]).unwrap();
+    let pubqty = Fp::from(1 as u64).into();
+    let pubsuite = Fp::from(2 as u64).into();
+    let pubrank = Fp::from(3 as u64).into();
+
+    let public_inputs = vec![vec![pubqty], vec![pubsuite], vec![pubrank]];
+    let prover = MockProver::run(k, &circuit, public_inputs).unwrap();
     prover.assert_satisfied();
 }
